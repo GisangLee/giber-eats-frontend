71a17ddabdff7d94f4b100beb8cae3bb503bc934
GisangLee <masterkorea01@naver.com>
2022-06-21 16:40:14 +0900

    search and category

--- src/pages/client/category.tsx
+++ src/pages/client/category.tsx
@@ -1,6 +1,9 @@
 import { gql, useQuery } from '@apollo/client';
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { Helmet } from 'react-helmet-async';
+import { Link } from 'react-router-dom';
 import { useLocation, useParams } from 'react-router-dom';
+import { Restaurant } from '../../components/restaurant';
 import { CATEGORY_FRAGMENT, RESTAURANT_FRAGMENT } from '../../fragments';
 import { categoryQuery, categoryQueryVariables } from '../../__generated__/categoryQuery';
 
@@ -31,8 +34,12 @@ type ICategoryParams = {
 export const Category = () => {
 
     const params = useParams<ICategoryParams>();
+    const [page, setPage] = useState(1);
 
-    const { data, loading } = useQuery<categoryQuery, categoryQueryVariables>(CATEGORY_QUERY, {
+    const onNextPage = () => setPage(current => current + 1);
+    const onPrevPage = () => setPage(current => current - 1);
+
+    const { data:categoryQueryResult , loading } = useQuery<categoryQuery, categoryQueryVariables>(CATEGORY_QUERY, {
         variables: {
             input: {
                 page: 1,
@@ -41,10 +48,44 @@ export const Category = () => {
         },
     });
 
-    console.log(data);
-
     
     return (
-        <div>Cate</div>
+        <>
+            <Helmet>
+                <title>카테고리 | Giber Eats</title>
+            </Helmet>
+
+            { !loading && categoryQueryResult && categoryQueryResult.category.restaurants && (
+                <div className='max-w-screen-2xl mx-auto mt-8 pb-20'>
+                    <div className='grid md:grid-cols-3 gap-x-5 gap-y-7 mt-16'>
+                    { categoryQueryResult.category.restaurants.map(restaurant => {
+                        if(restaurant.category) {
+                            return (
+                                <Link key={ restaurant.id } to={ `/restaurant/${ restaurant.id }` }>
+                                    <Restaurant coverImg={ restaurant.coverImg } id={ restaurant.id + "" } name={ restaurant.name } categoryName={ restaurant.category.name }/>
+                                </Link>
+                            );
+                        }else if (!restaurant.category) {
+                            return (
+                                <Link key={ restaurant.id } to={ `/restaurant/${ restaurant.id }` }>
+                                    <Restaurant coverImg={ restaurant.coverImg } id={ restaurant.id + "" } name={ restaurant.name }/>
+                                </Link>
+                            );
+                        }
+                    })}
+                    </div>
+
+                    <div className='grid grid-cols-3 text-center max-w-md items-center mx-auto mt-10'>
+                    { page > 1 ? <button className='focus:outline-none font-medium text-2xl' onClick={ onNextPage }>&larr;</button> : <div></div> }
+
+                    <span className='mx-5'>페이지 { page } of { categoryQueryResult.category.totalPages }</span>
+
+                    { page !== categoryQueryResult.category.totalPages ? (
+                        <button className='focus:outline-none font-medium text-2xl' onClick={ onPrevPage }>&rarr;</button>
+                    ) : <div></div> }
+                    </div>
+                </div>
+            )}
+        </>
     );
 };
--- src/pages/client/search.tsx
+++ src/pages/client/search.tsx
@@ -6,6 +6,8 @@ import { useQueryParam } from '../../hooks/useQueryParam';
 import { gql, useLazyQuery } from '@apollo/client';
 import { RESTAURANT_FRAGMENT } from '../../fragments';
 import { searchRestaurant, searchRestaurantVariables } from '../../__generated__/searchRestaurant';
+import { Restaurant } from '../../components/restaurant';
+import { Link } from 'react-router-dom';
 
 const SEARCH_RESTAURANT = gql`
     query searchRestaurant($input: SearchRestaurantInput!) {
@@ -58,17 +60,35 @@ export const Search = () => {
             <Helmet>
                 <title>검색 | Giber Eats</title>
             </Helmet>
-            <div>searchTerm</div>
-
+            
             { !loading && searchRestaurantResult && searchRestaurantResult.searchRestaurant.restaurants && (
-                <div className='grid grid-cols-3 text-center max-w-md items-center mx-auto mt-10'>
-                    { page > 1 ? <button className='focus:outline-none font-medium text-2xl' onClick={ onNextPage }>&larr;</button> : <div></div> }
+                <div className='max-w-screen-2xl mx-auto mt-8 pb-20'>
+                    <div className='grid md:grid-cols-3 gap-x-5 gap-y-7 mt-16'>
+                    { searchRestaurantResult.searchRestaurant.restaurants.map(restaurant => {
+                        if(restaurant.category) {
+                            return (
+                                <Link key={ restaurant.id } to={ `/restaurant/${ restaurant.id }` }>
+                                    <Restaurant coverImg={ restaurant.coverImg } id={ restaurant.id + "" } name={ restaurant.name } categoryName={ restaurant.category.name }/>
+                                </Link>
+                            );
+                        }else if (!restaurant.category) {
+                            return (
+                                <Link key={ restaurant.id } to={ `/restaurant/${ restaurant.id }` }>
+                                    <Restaurant coverImg={ restaurant.coverImg } id={ restaurant.id + "" } name={ restaurant.name }/>
+                                </Link>
+                            );
+                        }
+                    })}
+                    </div>
+                    <div className='grid grid-cols-3 text-center max-w-md items-center mx-auto mt-10'>
+                        { page > 1 ? <button className='focus:outline-none font-medium text-2xl' onClick={ onNextPage }>&larr;</button> : <div></div> }
 
-                    <span className='mx-5'>페이지 { page } of { searchRestaurantResult.searchRestaurant.totalPages }</span>
+                        <span className='mx-5'>페이지 { page } of { searchRestaurantResult.searchRestaurant.totalPages }</span>
 
-                    { page !== searchRestaurantResult.searchRestaurant.totalPages ? (
-                        <button className='focus:outline-none font-medium text-2xl' onClick={ onPrevPage }>&rarr;</button>
-                    ) : <div></div> }
+                        { page !== searchRestaurantResult.searchRestaurant.totalPages ? (
+                            <button className='focus:outline-none font-medium text-2xl' onClick={ onPrevPage }>&rarr;</button>
+                        ) : <div></div> }
+                    </div>
                 </div>
             )}
         </>
