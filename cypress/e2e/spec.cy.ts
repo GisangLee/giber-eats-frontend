668c6e5bf49310fedc51f401ceb5af0a8aa4d9c7
GisangLee <masterkorea01@naver.com>
2022-06-22 17:09:12 +0900

    login cypress test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,24 +1,51 @@
 import { createVerify } from "crypto";
+import { isTypedArray } from "util/types";
 
 describe("Log In", () => {
+  const user = cy;
   it("See Log in page", () => {
-    cy.visit("/").title().should("eq", "로그인 | Giber Eats");
+    user.visit("/").title().should("eq", "로그인 | Giber Eats");
   });
-  
+
+  it("See email and password validation errors", () => {
+    user.visit("/")
+    user.findByPlaceholderText("이메일").type("test@.com")
+    user.findByRole("alert").should("have.text", "이메일 형식이 아닙니다.")
+
+    user.findByPlaceholderText("비밀번호").type("qwe")
+    user.get('.grid > :nth-child(4)').should("have.text", "최소 8자리 이상이어야 합니다.")
+
+    user.findByPlaceholderText("이메일").clear()
+    //user.findByRole("alert").should("have.text", "필수항목입니다.")
+    user.get('.grid > :nth-child(1)').should("have.text", "필수항목입니다.")
+
+    user.findByPlaceholderText("비밀번호").clear()
+    user.get('.grid > :nth-child(4)').should("have.text", "필수항목입니다.")
+  });
+
   it("can fill out form", () => {
-    cy.visit("/")
-    .get('[name="email"]').type("test@gmail.com")
-    .get('[name="password"]').type("qwe12312312312321")
-    .get('.text-lg').should("not.have.class", "pointer-events-none");
+    user.visit("/");
+    //user.get('[name="email"]').type("test@gmail.com")
+    user.findByPlaceholderText("이메일").type("test@gmail.com")
+    user.findByPlaceholderText("비밀번호").type("qweqweqweqweqwqw")
+    //user.get('[name="password"]').type("qwe12312312312321")
+    user.findByRole("button").should("not.have.class", "pointer-events-none");
+    
+  });
+
+  it("Log in", () => {
+    user.visit("/");
+    //user.get('[name="email"]').type("test@gmail.com")
+    user.findByPlaceholderText("이메일").type("masterkorea01@naver.com")
+    user.findByPlaceholderText("비밀번호").type("qwe123qwe123")
+    //user.get('[name="password"]').type("qwe12312312312321")
+    user.findByRole("button").should("not.have.class", "pointer-events-none").click()
+    user.window().its("localStorage.giber-token").should("be.a", "string")
 
     //TODO: can log in
   });
 
-  it("See email and password validation errors", () => {
-    cy.visit("/")
-    .get('[name="email"]').type("bad-email")
-    .get('.grid > :nth-child(1)').should("have.text", "이메일 형식이 아닙니다.")
-    .get('[name="password"]').type("qwe1")
-    .get('.grid > :nth-child(4)').should("have.text", "최소 8자리 이상이어야 합니다.");
+  it("sign up", () => {
+    user.visit("/create-account");
   });
 });
