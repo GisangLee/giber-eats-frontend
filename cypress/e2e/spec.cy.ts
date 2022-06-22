4ef4b37c3d7f32697512f7b4114ced1c1fa1e216
GisangLee <masterkorea01@naver.com>
2022-06-22 16:38:02 +0900

    cypress settings

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,24 @@
-describe("first test", () => {
-  it("should go to home ", () => {
-    cy.visit("http://localhost:3000").title().should("eq", "로그인 | Giber Eats");
+import { createVerify } from "crypto";
+
+describe("Log In", () => {
+  it("See Log in page", () => {
+    cy.visit("/").title().should("eq", "로그인 | Giber Eats");
+  });
+  
+  it("can fill out form", () => {
+    cy.visit("/")
+    .get('[name="email"]').type("test@gmail.com")
+    .get('[name="password"]').type("qwe12312312312321")
+    .get('.text-lg').should("not.have.class", "pointer-events-none");
+
+    //TODO: can log in
+  });
+
+  it("See email and password validation errors", () => {
+    cy.visit("/")
+    .get('[name="email"]').type("bad-email")
+    .get('.grid > :nth-child(1)').should("have.text", "이메일 형식이 아닙니다.")
+    .get('[name="password"]').type("qwe1")
+    .get('.grid > :nth-child(4)').should("have.text", "최소 8자리 이상이어야 합니다.");
   });
 });
