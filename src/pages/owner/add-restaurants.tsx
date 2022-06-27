98e6b0f0373d7a84adbeef7b2eb13d25a927c8a8
GisangLee <masterkorea01@naver.com>
2022-06-27 13:28:43 +0900

    file

--- src/pages/owner/add-restaurants.tsx
+++ src/pages/owner/add-restaurants.tsx
@@ -1,5 +1,5 @@
 import { gql, useMutation } from '@apollo/client';
-import React from 'react'
+import React, { useState } from 'react'
 import { Helmet } from 'react-helmet-async';
 import { useForm } from 'react-hook-form';
 import { Button } from '../../components/button';
@@ -18,18 +18,57 @@ interface IFormProps {
     name: string;
     address: string;
     categoryName: string;
+    file:FileList;
 };
 
 export const AddRestaurants = () => {
+    const [uploading, setUploading] = useState(false);
 
-    const [ createRestaurantMutation, { loading, error, data:createRestaurantResult }] = useMutation<createRestaurant, createRestaurantVariables>(CREATE_RESTAURANT_MUTATION);  
+    const onCompleted = (data:createRestaurant) => {
+        const { createRestaurant: { ok, error } } = data;
+        if (ok) {
+            setUploading(false);
+        }
+    };
+
+    const [ createRestaurantMutation, { loading, error, data:createRestaurantResult }] = useMutation<createRestaurant, createRestaurantVariables>(CREATE_RESTAURANT_MUTATION, {
+        onCompleted
+    });
 
     const { register, getValues, formState: { errors, isValid }, handleSubmit } = useForm<IFormProps>({
         mode: "onChange"
     });
 
-    const onSubmit = () => {
-        console.log(getValues());
+    const onSubmit = async () => {
+        try {
+            setUploading(true);
+
+            const { file, name, categoryName, address } = getValues();
+            const actualFile = file[0];
+    
+            const formBody = new FormData();
+    
+            formBody.append("file", actualFile);
+    
+            const { url: coverImg } = await (
+                await fetch("http://localhost:4000/uploads/", {
+                    method: "POST",
+                    body: formBody
+                })
+            ).json();
+            createRestaurantMutation({
+                variables: {
+                    input: {
+                        name,
+                        address,
+                        categoryName,
+                        coverImg
+                    },
+                },
+            });
+        } catch (e) {
+            console.log(e);
+        }
     };
 
     return (
@@ -42,9 +81,11 @@ export const AddRestaurants = () => {
                 <input className='input' { ...register("name", { required: "필수항목입니다."} )} placeholder='name' name="name" type="text"/>
                 <input className='input' { ...register("address", { required: "필수 항목입니다." } ) } placeholder='address' name="address" type="text"/>
                 <input className='input' { ...register("categoryName", { required: "필수 항목입니다." } ) } placeholder='categoryName' name="categoryName" type="text"/>
-                <Button loading={ loading } canClick={ isValid } actionText={ "가게 생성하기" }/>
+                <div>
+                    <input { ...register("file", { required: "필수 항목입니다. " } ) } type="file" name="file" accept='image/*' />
+                </div>
+                <Button loading={ uploading } canClick={ isValid } actionText={ "가게 생성하기" }/>
             </form>
         </div>
     );
-
 };
